bb2fdc5cc3975a0c27b5ec2981f25cdf2c7ed994
shem <wmlzjb@aliyun.com>
2016-06-02 14:21:19 +0800

    shem【完善目录结构】

--- gulp/tasks/pc-app-dev.js
+++ gulp/tasks/pc-app-dev.js
@@ -5,9 +5,9 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
-var webpackConfig = require("../../one-app/webpack.config.js");
+var webpackConfig = require("../../pc-app/webpack.config.js");
 
-gulp.task("one-app-dev", function (callback) {
+gulp.task("pc-app-dev", function (callback) {
     var myConfig = Object.create(webpackConfig);
     webpack(
         myConfig
@@ -20,13 +20,13 @@ gulp.task("one-app-dev", function (callback) {
         });
 });
 
-gulp.task("one-app-dev-server", function(callback) {
+gulp.task("pc-app-dev-server", function(callback) {
     var myConfig = Object.create(webpackConfig);
     myConfig.devtool = 'eval';
     myConfig.debug = true;
     var compiler = webpack(myConfig);
     new WebpackDevServer(compiler, {
-        contentBase:'./one-app/src/',
+        contentBase:'./pc-app/src/',
         inline:true
     }).listen(2995, "localhost", function(err) {
         if(err) throw new gutil.PluginError("webpack-dev-server", err);
---
+++ pc-app/src/module/login/module.js
@@ -0,0 +1,3 @@
+/**
+ * Created by shem on 2016/6/2.
+ */
