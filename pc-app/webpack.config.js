cf3edb32db1b6aceed86eae9046dbf7083e693fe
shem <wmlzjb@aliyun.com>
2016-06-02 16:51:59 +0800

    shem【完善目录结构】

--- pc-app/webpack.config.js
+++ pc-app/webpack.config.js
@@ -2,6 +2,7 @@
  * Created by shem on 2016/6/1.
  */
 var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     devtool: 'source-map',
     entry: __dirname + '/src/app.js',
@@ -12,12 +13,15 @@ module.exports = {
     module: {
         //各种加载器，即让各种文件格式可用require引用
         loaders: [
-            {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.scss$/, loader: ExtractTextPlugin.extract('css?sourceMap!' + 'sass?sourceMap')},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')},
             {test: /\.woff|\.woff2|\.svg|.eot|\.ttf/, loader: 'url?prefix=font/&limit=10000'},
             {test: /\.html$/, loader: 'file?name=html-[hash:6].html'},
             {test: /\.(png|jpg|ttf)$/, loader: 'url?limit=8192'}
             // { test: /\.less$/, loader: "style-loader!css-loader!less-loader"}
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("app.css")
+    ]
 };
